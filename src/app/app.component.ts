29c606e922cbd34aca65ed08d92e19c80eaf25ac
Mustafa Hossaini <mustafahossaini@gmail.com>
2017-08-30 18:39:47 +1200

    remove unused

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,7 @@ import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
   title = 'app';
