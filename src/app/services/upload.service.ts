a8548bfea31c5b1756f46aa5da6e33dec30b5dad
Mustafa Hossaini <mustafahossaini@gmail.com>
2017-09-05 13:15:33 +1200

    file upload

--- src/app/services/upload.service.ts
+++ src/app/services/upload.service.ts
@@ -3,18 +3,20 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Upload } from '../classes/upload';
 import * as firebase from 'firebase/app';
+import { UtilsService } from '../services/utils.service';
 
 @Injectable()
 export class UploadService {
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(private db: AngularFireDatabase, private utilsService: UtilsService) { }
 
   private basePath: string = '/uploads';
   uploads: FirebaseListObservable<Upload[]>;
 
   pushUpload(upload: Upload) {
     let storageRef = firebase.storage().ref();
-    let uploadTask = storageRef.child(this.basePath + '/' + upload.file.name).put(upload.file);
+
+    let uploadTask = storageRef.child(this.basePath + '/' + this.utilsService.uuidv4()).put(upload.file);
     upload.uploadTask = uploadTask;
 
     uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
