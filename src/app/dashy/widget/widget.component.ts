a4edd440e72d8a2a68b80b78b91d33729c9a11ec
Mustafa Hossaini <mustafahossaini@gmail.com>
2017-09-16 12:12:10 +1200

    dashy widget chart

--- src/app/dashy/widget/widget.component.ts
+++ src/app/dashy/widget/widget.component.ts
@@ -19,7 +19,40 @@ export class WidgetComponent implements OnInit {
   options: any = {
     title: { text: null },
     series: [{
-      data: [29.9, 71.5, 106.4, 129.2],
+      type: 'column',
+      name: 'Jane',
+      data: [3, 2, 1, 3, 4]
+    }, {
+      type: 'column',
+      name: 'John',
+      data: [2, 3, 5, 7, 6]
+    }, {
+      type: 'column',
+      name: 'Joe',
+      data: [4, 3, 3, 9, 0]
+    }, {
+      type: 'spline',
+      name: 'Average',
+      data: [3, 2.67, 3, 6.33, 3.33]
+    }, {
+      type: 'pie',
+      name: 'Total consumption',
+      data: [{
+        name: 'Jane',
+        y: 13
+      }, {
+        name: 'John',
+        y: 23
+      }, {
+        name: 'Joe',
+        y: 19
+      }],
+      center: [100, 80],
+      size: 100,
+      showInLegend: false,
+      dataLabels: {
+        enabled: false
+      }
     }],
     credits: {
       enabled: false
