09d45ff19a6f926fc8fdf4075e02df6c8c4fc18d
Mustafa Hossaini <mustafahossaini@gmail.com>
2017-09-19 15:59:28 +1200

    fcf timestamp on post and user

--- functions/index.js
+++ functions/index.js
@@ -5,6 +5,7 @@ admin.initializeApp(functions.config().firebase);
 exports.createProfile = functions.auth.user().onCreate(event => {
   return admin.database().ref(`/users/${event.data.uid}`).set({
     email: event.data.email,
+    createdAt: admin.database.ServerValue.TIMESTAMP,
     status: 'online'
   });
 });
@@ -12,4 +13,13 @@ exports.createProfile = functions.auth.user().onCreate(event => {
 exports.removeProfile = functions.auth.user().onDelete(event => {
   return admin.database().ref(`/users/${event.data.uid}`).remove();
 
+});
+
+exports.addPostTimeStamp = functions.database.ref('/posts/{id}').onWrite(ev => {
+  // prevent loops by guarding on data state
+  if (ev.data.child('createdAt').exists()) return;
+
+  return ev.data.adminRef.update({
+    createdAt: admin.database.ServerValue.TIMESTAMP
+  });
 });
