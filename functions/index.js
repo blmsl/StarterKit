32a6e503231f508a879f2b8a104dcbe2091bd2fa
Mustafa Hossaini <mustafahossaini@gmail.com>
2017-09-24 17:23:24 +1300

    fcf
    I created sendwelcomeemail and byeemail when user first signs up then
    deletes account.

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,11 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const gmailEmail = encodeURIComponent(functions.config().gmail.email);
+const gmailPassword = encodeURIComponent(functions.config().gmail.password);
+const mailTransport = nodemailer.createTransport('smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com');
+const APP_NAME = 'Cloud Storage for Firebase quickstart';
+
 admin.initializeApp(functions.config().firebase);
 
 exports.createProfile = functions.auth.user().onCreate(event => {
@@ -19,5 +25,41 @@ exports.addPostTimeStamp = functions.database.ref('/posts/{id}').onCreate(ev => 
 
   return ev.data.adminRef.update({
     createdAt: admin.database.ServerValue.TIMESTAMP
+  });
+});
+
+exports.sendWelcomeEmail = functions.auth.user().onCreate(event => {
+  const user = event.data;
+
+  const email = user.email;
+  const displayName = user.displayName || email;
+
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@firebase.com>`,
+    to: email
+  };
+
+  mailOptions.subject = `Welcome to ${APP_NAME}!`;
+  mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
+  return mailTransport.sendMail(mailOptions).then(() => {
+    console.log('New welcome email sent to:', email);
+  });
+});
+
+exports.sendByeEmail = functions.auth.user().onDelete(event => {
+  const user = event.data;
+
+  const email = user.email;
+  const displayName = user.displayName || email;
+
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@firebase.com>`,
+    to: email
+  };
+
+  mailOptions.subject = `Bye!`;
+  mailOptions.text = `Hey ${displayName || ''}!, We confirm that we have deleted your ${APP_NAME} account.`;
+  return mailTransport.sendMail(mailOptions).then(() => {
+    console.log('Account deletion confirmation email sent to:', email);
   });
 });
