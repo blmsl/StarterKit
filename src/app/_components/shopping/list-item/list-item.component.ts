85fac9b8b522e4cdc1192880bb071688a270beef
Mustafa Hossaini <mustafahossaini@gmail.com>
2017-10-01 23:20:26 +1300

    ecommerce thumbnail
    I added thumbnail control to the list item.
    In the future this can be changed with a timer like when you hover the
    list item.

--- src/app/_components/shopping/list-item/list-item.component.ts
+++ src/app/_components/shopping/list-item/list-item.component.ts
@@ -7,11 +7,33 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ListItemComponent implements OnInit {
   @Input() item: any;
-
+  currentThumbnail: number = 0;
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  thumbnailNext($event) {
+    this.currentThumbnail += 1;
+
+    if (this.currentThumbnail >= this.item.images.length) {
+      this.currentThumbnail = 0
+    }
+
+    $event.stopPropagation();
+    return false;
+  }
+
+  thumbnailPrev($event) {
+    this.currentThumbnail -= 1;
+
+    if (this.currentThumbnail < 0) {
+      this.currentThumbnail = this.item.images.length - 1;
+    }
+
+    $event.stopPropagation();
+    return false;
   }
 
 }
