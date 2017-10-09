eb338f4f448084526fd587964b5d72f69f102192
Mustafa Hossaini <mustafahossaini@gmail.com>
2017-10-09 21:49:36 +1300

    comment all functions

--- functions/index.js
+++ functions/index.js
@@ -8,58 +8,58 @@ const APP_NAME = 'App Demo by Mustafa';
 
 admin.initializeApp(functions.config().firebase);
 
-exports.createProfile = functions.auth.user().onCreate(event => {
-  return admin.database().ref(`/users/${event.data.uid}`).set({
-    email: event.data.email,
-    createdAt: admin.database.ServerValue.TIMESTAMP,
-    status: 'online'
-  });
-});
-
-exports.removeProfile = functions.auth.user().onDelete(event => {
-  return admin.database().ref(`/users/${event.data.uid}`).remove();
-
-});
-
-exports.addPostTimeStamp = functions.database.ref('/posts/{id}').onCreate(ev => {
-
-  return ev.data.adminRef.update({
-    createdAt: admin.database.ServerValue.TIMESTAMP
-  });
-});
-
-exports.sendWelcomeEmail = functions.auth.user().onCreate(event => {
-  const user = event.data;
-
-  const email = user.email;
-  const displayName = user.displayName || email;
-
-  const mailOptions = {
-    from: `${APP_NAME} <noreply@firebase.com>`,
-    to: email
-  };
-
-  mailOptions.subject = `Welcome to ${APP_NAME}!`;
-  mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
-  return mailTransport.sendMail(mailOptions).then(() => {
-    console.log('New welcome email sent to:', email);
-  });
-});
-
-exports.sendByeEmail = functions.auth.user().onDelete(event => {
-  const user = event.data;
-
-  const email = user.email;
-  const displayName = user.displayName || email;
-
-  const mailOptions = {
-    from: `${APP_NAME} <noreply@firebase.com>`,
-    to: email
-  };
-
-  mailOptions.subject = `Bye!`;
-  mailOptions.text = `Hey ${displayName || ''}!, We confirm that we have deleted your ${APP_NAME} account.`;
-  return mailTransport.sendMail(mailOptions).then(() => {
-    console.log('Account deletion confirmation email sent to:', email);
-  });
-});
+// exports.createProfile = functions.auth.user().onCreate(event => {
+//   return admin.database().ref(`/users/${event.data.uid}`).set({
+//     email: event.data.email,
+//     createdAt: admin.database.ServerValue.TIMESTAMP,
+//     status: 'online'
+//   });
+// });
+//
+// exports.removeProfile = functions.auth.user().onDelete(event => {
+//   return admin.database().ref(`/users/${event.data.uid}`).remove();
+//
+// });
+//
+// exports.addPostTimeStamp = functions.database.ref('/posts/{id}').onCreate(ev => {
+//
+//   return ev.data.adminRef.update({
+//     createdAt: admin.database.ServerValue.TIMESTAMP
+//   });
+// });
+//
+// exports.sendWelcomeEmail = functions.auth.user().onCreate(event => {
+//   const user = event.data;
+//
+//   const email = user.email;
+//   const displayName = user.displayName || email;
+//
+//   const mailOptions = {
+//     from: `${APP_NAME} <noreply@firebase.com>`,
+//     to: email
+//   };
+//
+//   mailOptions.subject = `Welcome to ${APP_NAME}!`;
+//   mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
+//   return mailTransport.sendMail(mailOptions).then(() => {
+//     console.log('New welcome email sent to:', email);
+//   });
+// });
+//
+// exports.sendByeEmail = functions.auth.user().onDelete(event => {
+//   const user = event.data;
+//
+//   const email = user.email;
+//   const displayName = user.displayName || email;
+//
+//   const mailOptions = {
+//     from: `${APP_NAME} <noreply@firebase.com>`,
+//     to: email
+//   };
+//
+//   mailOptions.subject = `Bye!`;
+//   mailOptions.text = `Hey ${displayName || ''}!, We confirm that we have deleted your ${APP_NAME} account.`;
+//   return mailTransport.sendMail(mailOptions).then(() => {
+//     console.log('Account deletion confirmation email sent to:', email);
+//   });
+// });
