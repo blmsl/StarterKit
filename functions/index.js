ffd48947a18f52cc5605cbb8a57eec364db951f4
Mustafa Hossaini <mustafahossaini@gmail.com>
2017-10-22 12:53:50 +1300

    enable welcome emails and by emails

--- functions/index.js
+++ functions/index.js
@@ -28,38 +28,39 @@ admin.initializeApp(functions.config().firebase);
 //   });
 // });
 //
-// exports.sendWelcomeEmail = functions.auth.user().onCreate(event => {
-//   const user = event.data;
-//
-//   const email = user.email;
-//   const displayName = user.displayName || email;
-//
-//   const mailOptions = {
-//     from: `${APP_NAME} <noreply@firebase.com>`,
-//     to: email
-//   };
-//
-//   mailOptions.subject = `Welcome to ${APP_NAME}!`;
-//   mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
-//   return mailTransport.sendMail(mailOptions).then(() => {
-//     console.log('New welcome email sent to:', email);
-//   });
-// });
-//
-// exports.sendByeEmail = functions.auth.user().onDelete(event => {
-//   const user = event.data;
-//
-//   const email = user.email;
-//   const displayName = user.displayName || email;
-//
-//   const mailOptions = {
-//     from: `${APP_NAME} <noreply@firebase.com>`,
-//     to: email
-//   };
-//
-//   mailOptions.subject = `Bye!`;
-//   mailOptions.text = `Hey ${displayName || ''}!, We confirm that we have deleted your ${APP_NAME} account.`;
-//   return mailTransport.sendMail(mailOptions).then(() => {
-//     console.log('Account deletion confirmation email sent to:', email);
-//   });
-// });
+
+exports.sendWelcomeEmail = functions.auth.user().onCreate(event => {
+  const user = event.data;
+
+  const email = user.email;
+  const displayName = user.displayName || email;
+
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@firebase.com>`,
+    to: email
+  };
+
+  mailOptions.subject = `Welcome to ${APP_NAME}!`;
+  mailOptions.text = `Hey ${displayName || ''}! Welcome to ${APP_NAME}. I hope you will enjoy our service.`;
+  return mailTransport.sendMail(mailOptions).then(() => {
+    console.log('New welcome email sent to:', email);
+  });
+});
+
+exports.sendByeEmail = functions.auth.user().onDelete(event => {
+  const user = event.data;
+
+  const email = user.email;
+  const displayName = user.displayName || email;
+
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@firebase.com>`,
+    to: email
+  };
+
+  mailOptions.subject = `Bye!`;
+  mailOptions.text = `Hey ${displayName || ''}!, We confirm that we have deleted your ${APP_NAME} account.`;
+  return mailTransport.sendMail(mailOptions).then(() => {
+    console.log('Account deletion confirmation email sent to:', email);
+  });
+});
