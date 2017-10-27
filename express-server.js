2d12ccadf24372c1c186fade68b1689e97cab31c
Mustafa Hossaini <mustafahossaini@gmail.com>
2017-10-28 01:33:17 +1300

    express-server fix

--- express-server.js
+++ express-server.js
@@ -2,26 +2,6 @@
 const express = require('express');
 const app = express();
 
-
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-// PathLocationStrategy
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
-
-
-
 // If an incoming request uses
 // a protocol other than HTTPS,
 // redirect that request to the
@@ -36,3 +16,21 @@ const forceSSL = function() {
     next();
   }
 }
+
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// PathLocationStrategy
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
