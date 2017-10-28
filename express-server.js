c01b11e82207df9c94cff41c10044e69b6f04c03
Mustafa Hossaini <mustafahossaini@gmail.com>
2017-10-28 22:56:38 +1300

    Fix heroku routing problem

--- express-server.js
+++ express-server.js
@@ -27,7 +27,7 @@ app.use(forceSSL());
 app.use(express.static(__dirname + '/dist'));
 
 // PathLocationStrategy
-app.get('/*', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
